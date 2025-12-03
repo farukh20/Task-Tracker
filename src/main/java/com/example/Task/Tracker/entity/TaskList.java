1f96da4c40e9caa9c580730e88129c29b7fa52d6
Farukh <xfarukh.3@gmail.com>
2025-12-04 00:10:01 +0530

    added TaskList entity

---
+++ src/main/java/com/example/Task/Tracker/entity/TaskList.java
@@ -0,0 +1,34 @@
+package com.example.Task.Tracker.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data //getter+setter+hashcode+tostring+equals
+public class TaskList {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(updatable = false,nullable = false)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "created_date",nullable = false)
+    private LocalDateTime createdDate;
+
+    @Column(name = "updated_date",nullable = false)
+    private LocalDateTime updatedDate;
+
+}
