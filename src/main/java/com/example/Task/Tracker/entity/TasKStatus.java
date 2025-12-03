7aa7996f06a13162871634cad38624ceb1b828c9
Farukh <xfarukh.3@gmail.com>
2025-12-03 23:59:52 +0530

    added enums TaskPriority and TasKStatus

---
+++ src/main/java/com/example/Task/Tracker/entity/TasKStatus.java
@@ -0,0 +1,5 @@
+package com.example.Task.Tracker.entity;
+
+public enum TasKStatus {
+    OPEN,CLOSED
+}
---
+++ src/main/java/com/example/Task/Tracker/entity/TaskPriority.java
@@ -0,0 +1,5 @@
+package com.example.Task.Tracker.entity;
+
+public enum TaskPriority {
+    HIGH,MEDIUM,LOW
+}
