95058e630ff1d608223cef7fa8a1b106b0de1898
Farukh <xfarukh.3@gmail.com>
2025-12-03 23:28:58 +0530

    added  Dotenv dotenv = Dotenv.load(); System.setProperty("POSTGRES_PASSWORD", dotenv.get("POSTGRES_PASSWORD"));

--- src/main/java/com/example/Task/Tracker/TaskTrackerApplication.java
+++ src/main/java/com/example/Task/Tracker/TaskTrackerApplication.java
@@ -1,5 +1,6 @@
 package com.example.Task.Tracker;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TaskTrackerApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TaskTrackerApplication.class, args);
+        Dotenv dotenv = Dotenv.load();
+        // Expose .env values to Spring Boot
+        System.setProperty("POSTGRES_PASSWORD", dotenv.get("POSTGRES_PASSWORD"));
+        SpringApplication.run(TaskTrackerApplication.class, args);
 	}
 
 }
